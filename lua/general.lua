2167ba14a5d583a89b5969a7ce3adc8ba65e5c1a
Marko Jovanovic <marko@mikroe.com>
2024-03-14 15:29:24 +0100

    added word selection grep for telescope

--- lua/general.lua
+++ lua/general.lua
@@ -24,3 +24,16 @@ api.nvim_create_autocmd("TextYankPost", {
         })
     end,
 })
+-- get visual selection
+function vim.getVisualSelection()
+	vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg("v")
+	vim.fn.setreg("v", {})
+
+	text = string.gsub(text, "\n", "")
+	if #text > 0 then
+		return text
+	else
+		return ""
+	end
+end
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,52 +1,63 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            -- init builtin
-            local builtin = require("telescope.builtin")
-            -- set keymaps
-            vim.keymap.set('n', '<leader>ff', builtin.find_files, {
-                desc = "Find files [Telescope]"
-            })
-            vim.keymap.set('n', '<leader>fp', builtin.git_files, {
-                desc = "Find Git files [Telescope]"
-            })
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {
-                desc = "Live file grep [Telescope]"
-            })
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {
-                desc = "Buffers [Telescope]"
-            })
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {
-                desc = "Help tags [Telescope]"
-            })
-            vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {
-                desc = "LSP Document symbols [Telescope]"
-            })
-            vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {
-                desc = "Old files [Telescope]"
-            })
-            vim.keymap.set('n', '<leader>fw',
-                function()
-                    builtin.grep_string({
-                        search = vim.fn.input("Find word > ")
-                    })
-                end, { desc = "Find word [Telescope]" })
-        end
-    },
-    {
-        'nvim-telescope/telescope-ui-select.nvim',
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {
-                        }
-                    }
-                }
-            })
-            require("telescope").load_extension("ui-select")
-        end
-    }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			-- init builtin
+			local builtin = require("telescope.builtin")
+			-- set keymaps
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {
+				desc = "Find files [Telescope]",
+			})
+			vim.keymap.set("v", "<leader>ff", function()
+				local text = vim.getVisualSelection()
+				builtin.find_files({ default_text = text })
+			end, {
+				desc = "Find selection in files [Telescope]",
+			})
+			vim.keymap.set("n", "<leader>fp", builtin.git_files, {
+				desc = "Find Git files [Telescope]",
+			})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
+				desc = "Live file grep [Telescope]",
+			})
+			vim.keymap.set("v", "<leader>fg", function()
+				local text = vim.getVisualSelection()
+				builtin.live_grep({ default_text = text })
+			end, {
+				desc = "Live file grep selection [Telescope]",
+			})
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+				desc = "Buffers [Telescope]",
+			})
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
+				desc = "Help tags [Telescope]",
+			})
+			vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {
+				desc = "LSP Document symbols [Telescope]",
+			})
+			vim.keymap.set("n", "<leader>fr", builtin.oldfiles, {
+				desc = "Old files [Telescope]",
+			})
+			vim.keymap.set("n", "<leader>fw", function()
+				builtin.grep_string({
+					search = vim.fn.input("Find word > "),
+				})
+			end, { desc = "Find word [Telescope]" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
