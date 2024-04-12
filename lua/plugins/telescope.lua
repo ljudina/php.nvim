dc02086635cb215be10c5ea6c11795b865ed5b2a
Marko Jovanovic <marko@mikroe.com>
2024-04-12 12:58:51 +0200

    telescope changed from tag to branch

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return {
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.x",
+        event = 'VimEnter',
+        branch = '0.1.x',
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             -- init builtin
