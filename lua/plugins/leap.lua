b5c920208917f404c4cc14709ea519452b85f84e
Marko Jovanovic <marko@mikroe.com>
2024-05-07 15:34:07 +0200

    changed from ctrl to alt for leap search

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,7 +2,7 @@ return {
     "ggandor/leap.nvim",
     config = function()
         require("leap")
-        vim.keymap.set("n", "<C-s>", function()
+        vim.keymap.set("n", "<A-s>", function()
             local current_window = vim.fn.win_getid()
             require("leap").leap({ target_windows = { current_window } })
         end, {
