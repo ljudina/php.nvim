473874422591c1bf45b3a5dcec8c5edd820a6bc1
Marko Jovanovic <marko@mikroe.com>
2024-05-07 11:43:53 +0200

    added leap for better searching

---
+++ lua/plugins/leap.lua
@@ -0,0 +1,13 @@
+return {
+    "ggandor/leap.nvim",
+    config = function()
+        require("leap")
+        vim.keymap.set("n", "<C-s>", function()
+            local current_window = vim.fn.win_getid()
+            require("leap").leap({ target_windows = { current_window } })
+        end, {
+            silent = true,
+            desc = "Search with leap",
+        })
+    end,
+}
