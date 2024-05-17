c0dabf4801529c7864352122588d4ece4dc0e266
Marko Jovanovic <marko@mikroe.com>
2024-05-17 11:55:23 +0200

    changed dap ui layout

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,234 +1,253 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		{ "nvim-neotest/nvim-nio" },
-		-- fancy UI for the debugger
-		{
-			"rcarriga/nvim-dap-ui",
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        { "nvim-neotest/nvim-nio" },
+        -- fancy UI for the debugger
+        {
+            "rcarriga/nvim-dap-ui",
             -- stylua: ignore
             keys = {
                 { "<leader>du", function() require("dapui").toggle({}) end, desc = "Dap UI" },
                 { "<leader>de", function() require("dapui").eval() end,     desc = "Eval",  mode = { "n", "v" } },
             },
-			opts = {},
-			config = function(_, opts)
-				-- setup dap config by VsCode launch.json file
-				-- require("dap.ext.vscode").load_launchjs()
-				local dap = require("dap")
-				local dapui = require("dapui")
-				dapui.setup(opts)
-				dap.listeners.before.attach.dapui_config = function()
-					dapui.open()
-				end
-				dap.listeners.before.launch.dapui_config = function()
-					dapui.open()
-				end
-				dap.listeners.before.event_terminated.dapui_config = function()
-					dapui.close()
-				end
-				dap.listeners.before.event_exited.dapui_config = function()
-					dapui.close()
-				end
-			end,
-		},
+            config = function()
+                -- setup dap config by VsCode launch.json file
+                -- require("dap.ext.vscode").load_launchjs()
+                local dap = require("dap")
+                local dapui = require("dapui")
+                dapui.setup({
+                    layouts = {
+                        {
+                            elements = {
+                                {
+                                    id = "breakpoints",
+                                    size = 0.15
+                                },
+                                {
+                                    id = "scopes",
+                                    size = 0.85
+                                },
+                                -- "stacks",
+                                -- "watches",
+                            },
+                            size = 80,
+                            position = "left",
+                        },
+                    },
+                    windows = { indent = 1 },
+                })
+                dap.listeners.before.attach.dapui_config = function()
+                    dapui.open()
+                end
+                dap.listeners.before.launch.dapui_config = function()
+                    dapui.open()
+                end
+                dap.listeners.before.event_terminated.dapui_config = function()
+                    dapui.close()
+                end
+                dap.listeners.before.event_exited.dapui_config = function()
+                    dapui.close()
+                end
+            end,
+        },
 
-		-- virtual text for the debugger
-		{
-			"theHamsta/nvim-dap-virtual-text",
-			opts = {},
-		},
+        -- virtual text for the debugger
+        {
+            "theHamsta/nvim-dap-virtual-text",
+            opts = {},
+        },
 
-		-- mason.nvim integration
-		{
-			"jay-babu/mason-nvim-dap.nvim",
-			dependencies = "mason.nvim",
-			cmd = { "DapInstall", "DapUninstall" },
-			opts = {
-				-- Makes a best effort to setup the various debuggers with
-				-- reasonable debug configurations
-				automatic_installation = true,
+        -- mason.nvim integration
+        {
+            "jay-babu/mason-nvim-dap.nvim",
+            dependencies = "mason.nvim",
+            cmd = { "DapInstall", "DapUninstall" },
+            opts = {
+                -- Makes a best effort to setup the various debuggers with
+                -- reasonable debug configurations
+                automatic_installation = true,
 
-				-- You can provide additional configuration to the handlers,
-				-- see mason-nvim-dap README for more information
-				handlers = {},
+                -- You can provide additional configuration to the handlers,
+                -- see mason-nvim-dap README for more information
+                handlers = {},
 
-				-- You'll need to check that you have the required things installed
-				-- online, please don't ask me how to install them :)
-				ensure_installed = {
-					-- Update this to ensure that you have the debuggers for the langs you want
-				},
-			},
-		},
-	},
-	config = function()
-		local dap = require("dap")
-		local currentOs = os.getenv("HOME")
-		dap.adapters.php = function(callback, config)
-			callback({
-				type = "executable",
-				command = "node",
-				args = {
-					currentOs .. "/vscode-php-debug/out/phpDebug.js",
-				},
-				cwd = "${workspaceFolder}",
-			})
-		end
-		dap.configurations.php = {
-			{
-				type = "php",
-				request = "launch",
-				name = "Listen for XDebug on Docker App",
-				port = 9000,
-				pathMappings = {
-					["/var/www/html"] = "${workspaceFolder}",
-				},
-			},
-		}
-	end,
-	keys = {
-		{
-			"<leader>dB",
-			function()
-				require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
-			end,
-			desc = "Breakpoint Condition",
-		},
-		{
-			"<leader>dt",
-			function()
-				require("dapui").close()
-				require("dap").terminate()
-			end,
-			desc = "Terminate",
-		},
-		{
-			"<F9>",
-			function()
-				require("dap").toggle_breakpoint()
-			end,
-			desc = "Toggle Breakpoint",
-		},
-		{
-			"<F5>",
-			function()
-				require("dap").continue()
-			end,
-			desc = "Continue",
-		},
-		{
-			"<C-F10>",
-			function()
-				require("dap").run_to_cursor()
-			end,
-			desc = "Run to Cursor",
-		},
-		{
-			"<leader>dg",
-			function()
-				require("dap").goto_()
-			end,
-			desc = "Go to line (no execute)",
-		},
-		{
-			"<F11>",
-			function()
-				require("dap").step_into()
-			end,
-			desc = "Step Into",
-		},
-		{
-			"<leader>dj",
-			function()
-				require("dap").down()
-			end,
-			desc = "Down",
-		},
-		{
-			"<leader>dk",
-			function()
-				require("dap").up()
-			end,
-			desc = "Up",
-		},
-		{
-			"<leader>dl",
-			function()
-				require("dap").run_last()
-			end,
-			desc = "Run Last",
-		},
-		{
-			"<S-F11>",
-			function()
-				require("dap").step_out()
-			end,
-			desc = "Step Out",
-		},
-		{
-			"<F10>",
-			function()
-				require("dap").step_over()
-			end,
-			desc = "Step Over",
-		},
-		{
-			"<leader>dp",
-			function()
-				require("dap").pause()
-			end,
-			desc = "Pause",
-		},
-		{
-			"<leader>dr",
-			function()
-				require("dap").repl.toggle()
-			end,
-			desc = "Toggle REPL",
-		},
-		{
-			"<leader>ds",
-			function()
-				require("dap").session()
-			end,
-			desc = "Session",
-		},
-		{
-			"<S-F5>",
-			function()
-				require("dapui").close()
-				require("dap").terminate()
-			end,
-			desc = "Terminate",
-		},
-		{
-			"<leader>dw",
-			function()
-				require("dap.ui.widgets").hover()
-			end,
-			desc = "Widgets - Hover",
-		},
-		{
-			"<leader>da",
-			function()
-				require("dap.ui.widgets").preview()
-			end,
-			desc = "Widgets - Preview",
-		},
-		{
-			"<leader>df",
-			function()
-				local widgets = require("dap.ui.widgets")
-				widgets.centered_float(widgets.frames)
-			end,
-			desc = "Widgets - Frames",
-		},
-		{
-			"<leader>dx",
-			function()
-				local widgets = require("dap.ui.widgets")
-				widgets.centered_float(widgets.scopes)
-			end,
-			desc = "Widgets - Scopes",
-		},
-	},
+                -- You'll need to check that you have the required things installed
+                -- online, please don't ask me how to install them :)
+                ensure_installed = {
+                    -- Update this to ensure that you have the debuggers for the langs you want
+                },
+            },
+        },
+    },
+    config = function()
+        local dap = require("dap")
+        local currentOs = os.getenv("HOME")
+        dap.adapters.php = function(callback, config)
+            callback({
+                type = "executable",
+                command = "node",
+                args = {
+                    currentOs .. "/vscode-php-debug/out/phpDebug.js",
+                },
+                cwd = "${workspaceFolder}",
+            })
+        end
+        dap.configurations.php = {
+            {
+                type = "php",
+                request = "launch",
+                name = "Listen for XDebug on Docker App",
+                port = 9000,
+                pathMappings = {
+                    ["/var/www/html"] = "${workspaceFolder}",
+                },
+            },
+        }
+    end,
+    keys = {
+        {
+            "<leader>dB",
+            function()
+                require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: "))
+            end,
+            desc = "Breakpoint Condition",
+        },
+        {
+            "<leader>dt",
+            function()
+                require("dapui").close()
+                require("dap").terminate()
+            end,
+            desc = "Terminate",
+        },
+        {
+            "<F9>",
+            function()
+                require("dap").toggle_breakpoint()
+            end,
+            desc = "Toggle Breakpoint",
+        },
+        {
+            "<F5>",
+            function()
+                require("dap").continue()
+            end,
+            desc = "Continue",
+        },
+        {
+            "<C-F10>",
+            function()
+                require("dap").run_to_cursor()
+            end,
+            desc = "Run to Cursor",
+        },
+        {
+            "<leader>dg",
+            function()
+                require("dap").goto_()
+            end,
+            desc = "Go to line (no execute)",
+        },
+        {
+            "<F11>",
+            function()
+                require("dap").step_into()
+            end,
+            desc = "Step Into",
+        },
+        {
+            "<leader>dj",
+            function()
+                require("dap").down()
+            end,
+            desc = "Down",
+        },
+        {
+            "<leader>dk",
+            function()
+                require("dap").up()
+            end,
+            desc = "Up",
+        },
+        {
+            "<leader>dl",
+            function()
+                require("dap").run_last()
+            end,
+            desc = "Run Last",
+        },
+        {
+            "<S-F11>",
+            function()
+                require("dap").step_out()
+            end,
+            desc = "Step Out",
+        },
+        {
+            "<F10>",
+            function()
+                require("dap").step_over()
+            end,
+            desc = "Step Over",
+        },
+        {
+            "<leader>dp",
+            function()
+                require("dap").pause()
+            end,
+            desc = "Pause",
+        },
+        {
+            "<leader>dr",
+            function()
+                require("dap").repl.toggle()
+            end,
+            desc = "Toggle REPL",
+        },
+        {
+            "<leader>ds",
+            function()
+                require("dap").session()
+            end,
+            desc = "Session",
+        },
+        {
+            "<S-F5>",
+            function()
+                require("dapui").close()
+                require("dap").terminate()
+            end,
+            desc = "Terminate",
+        },
+        {
+            "<leader>dw",
+            function()
+                require("dap.ui.widgets").hover()
+            end,
+            desc = "Widgets - Hover",
+        },
+        {
+            "<leader>da",
+            function()
+                require("dap.ui.widgets").preview()
+            end,
+            desc = "Widgets - Preview",
+        },
+        {
+            "<leader>df",
+            function()
+                local widgets = require("dap.ui.widgets")
+                widgets.centered_float(widgets.frames)
+            end,
+            desc = "Widgets - Frames",
+        },
+        {
+            "<leader>dx",
+            function()
+                local widgets = require("dap.ui.widgets")
+                widgets.centered_float(widgets.scopes)
+            end,
+            desc = "Widgets - Scopes",
+        },
+    },
 }
