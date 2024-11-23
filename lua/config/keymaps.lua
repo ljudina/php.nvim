48846321c503291818c362d1e3efdca1bf239559
Marko Jovanovic <marko@mikroe.com>
2024-11-23 16:23:20 +0100

    added shortcuts for copying filepaths

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,7 +14,7 @@ map("v", ">", ">gv")
 -- paste over currently selected text without yanking it
 map("v", "p", '"_dp')
 map("v", "P", '"_dP')
-
+-- cycle through buffers
 map("n", "<C-k>", "<cmd>bnext<CR>zz")
 map("n", "<C-j>", "<cmd>bprev<CR>zz")
 
@@ -22,3 +22,31 @@ map("n", "<leader>cn", "<cmd>cnext<CR>")
 map("n", "<leader>cp", "<cmd>cprev<CR>")
 map("n", "<leader>co", "<cmd>cope<CR>")
 map("n", "<leader>cc", "<cmd>ccl<CR>")
+
+
+vim.keymap.set("n", "<leader>ha", function()
+    local path = vim.fn.expand("%:p")
+    vim.fn.setreg("+", path)
+    vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {
+    silent = true,
+    desc = "Copy absolute path to clipboard",
+})
+
+vim.keymap.set("n", "<leader>hr", function()
+    local path = vim.fn.expand("%")
+    vim.fn.setreg("+", path)
+    vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {
+    silent = true,
+    desc = "Copy relative path to clipboard",
+})
+
+vim.keymap.set("n", "<leader>hf", function()
+    local path = vim.fn.expand("%:t")
+    vim.fn.setreg("+", path)
+    vim.notify('Copied "' .. path .. '" to the clipboard!')
+end, {
+    silent = true,
+    desc = "Copy filename to clipboard",
+})
