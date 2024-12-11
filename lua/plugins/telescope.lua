8dba2c3da41a752c2e8d7f20521322a63cb3d69f
Marko Jovanovic <marko@mikroe.com>
2024-12-11 09:40:49 +0100

    fzf extension enabled

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,11 +3,16 @@ return {
         "nvim-telescope/telescope.nvim",
         event = 'VimEnter',
         branch = '0.1.x',
-        dependencies = { 
+        dependencies = {
             "nvim-lua/plenary.nvim",
             { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
         },
         config = function()
+            require('telescope').setup {
+                extensions = {
+                    fzf = {}
+                }
+            }
             require('telescope').load_extension('fzf')
             -- init builtin
             local builtin = require("telescope.builtin")
