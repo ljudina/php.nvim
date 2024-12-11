7cee9a50f937b26d9238f70a6c5297e908d7946f
Marko Jovanovic <marko@mikroe.com>
2024-12-11 09:43:32 +0100

    telescope-ui-select removed because not used

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -99,17 +99,4 @@ return {
             end, { desc = "Find word [Telescope]" })
         end,
     },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-            require("telescope").load_extension("ui-select")
-        end,
-    },
 }
