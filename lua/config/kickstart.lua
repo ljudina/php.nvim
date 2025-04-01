5cef27711357614b11307ce9388eb330ae90ec20
Marko Jovanovic <marko@mikroe.com>
2025-04-01 15:43:19 +0200

    added diagnostic inline

--- lua/config/kickstart.lua
+++ lua/config/kickstart.lua
@@ -11,6 +11,7 @@ vim.opt.cursorline = true
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.diagnostic.config({ virtual_text = true })
 -- TIP: Disable arrow keys in normal mod
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
