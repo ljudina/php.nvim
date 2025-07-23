2995ad2c95dc3173f2195cc7e4bcfcc9ec26c5f7
Marko Jovanovic <marko@mikroe.com>
2025-07-23 10:32:48 +0200

    disable treesitter for large php files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,21 @@ return {
         local configTS = require("nvim-treesitter.configs")
         configTS.setup({
             auto_install = true,
-            highlight = { enable = true },
+            highlight = {
+                enable = true,
+                disable = function(lang, bufnr)
+                    if lang == "php" then -- treesitter breaks on large php files
+                        local max_filesize = 300 * 1024   -- 100 KB, adjust as needed
+                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+                        if ok and stats and stats.size > max_filesize then
+                            return true
+                        else
+                            return false
+                        end
+                    end
+                    return true
+                end,
+            },
             indent = { enable = true },
             ensure_installed = {
                 "bash",
