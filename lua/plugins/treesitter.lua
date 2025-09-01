3767f08518a53985a6cd8230f69da5958a4abd80
Marko Jovanovic <marko@mikroe.com>
2025-09-01 09:34:36 +0200

    removed disabling highlightning of large php files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,18 +8,6 @@ return {
             auto_install = true,
             highlight = {
                 enable = true,
-                disable = function(lang, bufnr)
-                    if lang == "php" then -- treesitter breaks on large php files
-                        local max_filesize = 300 * 1024   -- 100 KB, adjust as needed
-                        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-                        if ok and stats and stats.size > max_filesize then
-                            return true
-                        else
-                            return false
-                        end
-                    end
-                    return true
-                end,
             },
             indent = { enable = true },
             ensure_installed = {
