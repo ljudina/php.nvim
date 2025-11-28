176bdab866c71a5de92915489f2b6a007146291c
Marko Jovanovic <marko@mikroe.com>
2025-11-28 16:27:28 +0100

    blending opacity set

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,6 +1,10 @@
 return {
-  "j-hui/fidget.nvim",
-  opts = {
-    -- options
-  },
+    "j-hui/fidget.nvim",
+    opts = {
+        notification = {
+            window = {
+                winblend = 0,            -- Background color opacity in the notification window
+            },
+        },
+    },
 }
