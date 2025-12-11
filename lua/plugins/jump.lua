f9706805b2464c6e27a43169f8d3c9d30a53c6ca
Marko Jovanovic <marko@mikroe.com>
2025-12-11 09:49:35 +0100

    replaced flash with sneak for jump function

--- lua/plugins/jump.lua
+++ lua/plugins/jump.lua
@@ -1,14 +1,8 @@
 return {
-    "folke/flash.nvim",
-    event = "VeryLazy",
-    ---@type Flash.Config
-    opts = {},
-    -- stylua: ignore
-    keys = {
-        { "<A-s>",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
-        -- { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-        -- { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-        -- { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-        -- { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
-    },
+  "justinmk/vim-sneak",
+  event = "VeryLazy",
+  config = function()
+    vim.g["sneak#label"] = 1        -- show labels for targets
+    vim.g["sneak#use_ic_scs"] = 1   -- smartcase + ignorecase
+  end,
 }
