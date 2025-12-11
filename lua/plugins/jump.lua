2f9f758250c2801d474ca1b0b84c53b5c767a997
Marko Jovanovic <marko@mikroe.com>
2025-12-11 16:41:52 +0100

    changed keymap for sneak to leader s

--- lua/plugins/jump.lua
+++ lua/plugins/jump.lua
@@ -4,5 +4,11 @@ return {
   config = function()
     vim.g["sneak#label"] = 1        -- show labels for targets
     vim.g["sneak#use_ic_scs"] = 1   -- smartcase + ignorecase
+    vim.g["sneak#disable_default_mappings"] = 1 -- disable s / S mappings from Sneak
+    -- Custom mappings: Alt+s and Alt+S
+    vim.keymap.set({ "n", "x", "o" }, "<leader>s", "<Plug>Sneak_s")
+    vim.keymap.set({ "n", "x", "o" }, "<leader>S", "<Plug>Sneak_S")
+    -- Restore vim's default S behavior (change line)
+    vim.keymap.set("n", "S", "cc", { noremap = true })
   end,
 }
