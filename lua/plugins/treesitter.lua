dafed4e57d2b0a60b5fabc55c6268e2fbaa13693
Marko Jovanovic <ljudina@gmail.com>
2026-01-24 10:01:15 +0100

    update fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,40 +1,4 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
-        -- configure treesitter
-        local configTS = require("nvim-treesitter.configs")
-        configTS.setup({
-            auto_install = true,
-            highlight = {
-                enable = true,
-            },
-            indent = { enable = true },
-            ensure_installed = {
-                "bash",
-                "c",
-                "diff",
-                "html",
-                "php",
-                "javascript",
-                "jsdoc",
-                "json",
-                "jsonc",
-                "lua",
-                "luadoc",
-                "luap",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "regex",
-                "toml",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "yaml",
-            },
-        })
-    end,
 }
