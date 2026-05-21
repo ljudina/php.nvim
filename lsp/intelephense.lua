218bd1e039ad4e4a33685d929bdda335fb223103
Marko Jovanovic <marko@mikroe.com>
2026-05-21 10:12:22 +0200

    do not check for variable definition in cakephp view files

--- lsp/intelephense.lua
+++ lsp/intelephense.lua
@@ -1,3 +1,12 @@
+local function filter_ctp_undefined(err, result, ctx, config)
+    if result and result.uri and result.uri:sub(-4) == '.ctp' and result.diagnostics then
+        result.diagnostics = vim.tbl_filter(function(d)
+            return not (d.message and d.message:find('^Undefined variable'))
+        end, result.diagnostics)
+    end
+    return vim.lsp.handlers['textDocument/publishDiagnostics'](err, result, ctx, config)
+end
+
 return {
     settings = {
         intelephense = {
@@ -8,5 +17,8 @@ return {
         environment = {
             phpVersion = "5.6.0"
         },
+    },
+    handlers = {
+        ['textDocument/publishDiagnostics'] = filter_ctp_undefined,
     },
 }
